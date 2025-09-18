a330429de9d06fd1b090724787d75dd4c0dd8f49
whillingudw1337 <dorudavladyslav@gmail.com>
2025-09-18 20:56:26 +0200

    Add files via upload

---
+++ lekcja2.py
@@ -0,0 +1,15 @@
+liczba_int = 200 #int(
+liczba_zmienno_przecinkowa = 5.5 # float()
+ciag_znakow = "slowo" # str()
+prawda_falsz = True # False
+reprezentacja_niczego = None
+slownik_pusty = {} # dic()
+slownik = {"Klucz_1": "Wartosc_1", "Klucz_2": "Wartosc_2"}
+lista_pista = []
+lista = [1, 2, 3, "wiatrak"]
+zbior_pusty = set()
+zbior = {"jabko","jabko","jabko", "ser", "ser"}
+krotka_pusta = () #tuple ()
+krotka = (True, False, None)
+
+print(zbior,lista,liczba_int)
